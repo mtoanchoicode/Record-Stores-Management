6a3d602c5f0fcf37c87c530054c7780e80a0de17
Toan Truong <toantr.work@gmail.com>
2024-12-09 21:57:54 +0700

    Push Image to cloud

--- controllers/admin/product.controller.js
+++ controllers/admin/product.controller.js
@@ -36,11 +36,20 @@ module.exports.index = async (req, res)=>{
     }, req.query, countProducts
 
     )
+    //Sort
+    let sort = {}
+
+    if (req.query.sortKey && req.query.sortValue){
+        sort[req.query.sortKey] = req.query.sortValue
+    }else{
+        sort.position = "desc"
+    }
+
 
 
     const products = await Product
         .find(find)
-        .sort({position: "desc"})
+        .sort(sort)
         .limit(objectPagination.limitItems)
         .skip(objectPagination.skip);
 
